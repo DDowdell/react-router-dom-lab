2984bdcad354bf366c771da60aeede350fe47bc3
Dae Dowdell <dow_dell2010@yahoo.com>
2025-08-13 03:05:54 -0400

    initial steps completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-// src/App.jsx
+import NavBar from "./components/NavBar/NavBar";
+import MailboxForm from "./components/MailboxForm/MailboxForm";
+import MailboxList from "./components/MailboxList/MailboxList";
+import MailboxDetails from "./components/MailboxDetails/MailboxDetails";
+import React, { useState } from "react";
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [mailboxes, setMailboxes] = useState([]);
+
+  const addBox = (formData) => {
+    const newMailbox = {
+      _id: 1,
+      boxSize: 'Small',
+      boxOwner: 'Alex',
+    };
+    setMailboxes([...mailboxes, newMailbox]);
+  };
+
+  return (
+
+  );
+
 };
 
 export default App;
